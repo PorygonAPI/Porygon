b8f1d5a126a6379192383dff6076630d07952b80
João Arruda <joaoarruda.info@gmail.com>
2024-03-20 21:05:30 -0300

    iniciando projeto JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module br.com.porygon {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens br.com.porygon to javafx.fxml;
+    exports br.com.porygon;
+}
