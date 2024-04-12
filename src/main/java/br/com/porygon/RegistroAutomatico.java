5e408a817f1bc18cc29e68ad9b080db2bff041ff
Carla Daiane <daianecarla.222@gmail.com>
2024-04-12 13:48:37 -0300

    #por3: Criação do método 'converterRegistro' na classe 'RegistroAutomatico'.

--- src/main/java/br/com/porygon/RegistroAutomatico.java
+++ src/main/java/br/com/porygon/RegistroAutomatico.java
@@ -1,8 +1,30 @@
 package br.com.porygon;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegistroAutomatico extends Registro{
     public RegistroAutomatico(String data, String hora, double velVento, double temperatura) {
         super(data, hora, velVento, temperatura);
-        temperatura = temperatura - 273.15;
     }
+
+//    public double converterEscala() {
+//        // Obtém a temperatura da instância atual de RegistroAutomatico
+//        double temperaturaCelsius = getTemperatura() - 273.15;
+//        DecimalFormat df = new DecimalFormat("#.##");
+//        System.out.println("Temperatura registrada em Kelvin: " + df.format(getTemperatura()));
+//        System.out.println("Temperatura Convertida para Celsius: " + df.format(temperaturaCelsius));
+//        return temperaturaCelsius;
+//    }
+
+//    public static void main(String[] args) {
+//        // Criar uma instância da classe RegistroAutomatico
+//        RegistroAutomatico registroAuto = new RegistroAutomatico("01/01/2024", "12:00", 10.5, 293.75);
+//
+//        // Chamar o método converterEscala
+//        double temperaturaConvertida = registroAuto.converterEscala();
+//
+//    }
+
 }
