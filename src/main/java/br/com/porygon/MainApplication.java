8feba0eadf02695e6114655c244f3220a4a54c93
allineramos <144153142+allineramos@users.noreply.github.com>
2024-06-13 22:49:11 -0300

    Alteração de Label

--- src/main/java/br/com/porygon/MainApplication.java
+++ src/main/java/br/com/porygon/MainApplication.java
@@ -1,11 +1,11 @@
 package br.com.porygon;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
